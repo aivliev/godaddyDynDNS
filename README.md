01df0e10874dd3508c5a32c974aaa7c8b2b810b9
Aleksey Ivliev <aleksey.ivliev@burberry.com>
2017-11-10 11:21:05 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# godaddyDynDNS
+For now is just copypaste of 
+https://ru.godaddy.com/community/Managing-Domains/Dynamic-DNS-Updates/m-p/60614#M15277
+
+
+
+Before adding the cronjob, I put the script in /usr/local/bin, chowned it to nobody and chmoded it to 700. This was to prevent other users reading the script (to find the key and secret) or executing it (to update the records).
+ 
+I then ran touch /var/log/updatedomain.log and chowned it to nobody.
+ 
+Finally I edited nobody's crontab using crontab -e -u nobody and set it up as follows:
+#
+SHELL=/bin/sh
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/pkg/bin
+HOME=/var/tmp
+CRON_WITHIN=240
+#
+#minute hour    mday    month   wday    command
+#
+*/15    *       *       *       *       /usr/local/bin/updatedomain www mydomain.com >> /var/log/updatedomain.log
+You can, of course, substitute www for @ to update the actual domain's A record and add more lines to the crontab to update the A records of various different domains.
+ 
+I hope this helps somebody. Smiley Very Happy
---
+++ godaddyDynDNS.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+# This script is used to check and update your GoDaddy DNS server to the IP address of your current internet connection.
+#
+# Original PowerShell script by mfox: https://github.com/markafox/GoDaddy_Powershell_DDNS
+# Ported to bash by sanctus
+# Improved to take command line arguments and output information for log files by pollito
+#
+# First go to GoDaddy developer site to create a developer account and get your key and secret
+#
+# https://developer.godaddy.com/getstarted
+#
+# Be aware that there are 2 types of key and secret - one for the test server and one for the production server
+# Get a key and secret for the production server
+
+# Check an A record and a domain are both specified on the command line.
+
+if [ $# -ne 2 ]; then
+    echo "usage: updatedomain a_record domain_name"
+        exit 1
+fi
+
+# Set A record and domain to values specified by user
+
+name=$1     # name of A record to update
+domain=$2   # name of domain to update
+
+# Modify the next two lines with your key and secret
+
+key=""      # key for godaddy developer API
+secret=""   # secret for godaddy developer API
+
+headers="Authorization: sso-key $key:$secret"
+
+#echo $headers
+
+result=$(curl -s -k -X GET -H "$headers" \
+ "https://api.godaddy.com/v1/domains/$domain/records/A/$name")
+
+dnsIp=$(echo $result | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
+echo $name"."$domain": $(date): dnsIp:" $dnsIp
+
+# Get public ip address there are several websites that can do this.
+ret=$(curl -s GET "http://ipinfo.io/json")
+currentIp=$(echo $ret | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
+echo $name"."$domain": $(date): currentIp:" $currentIp
+
+ if [ $dnsIp != $currentIp ];
+ then
+        echo $name"."$domain": $(date): IPs not equal. Updating."
+        request='{"data":"'$currentIp'","ttl":3600}'
+        # echo $request
+        nresult=$(curl -i -k -s -X PUT \
+ -H "$headers" \
+ -H "Content-Type: application/json" \
+ -d $request "https://api.godaddy.com/v1/domains/$domain/records/A/$name")
+        # echo $nresult
+fi
